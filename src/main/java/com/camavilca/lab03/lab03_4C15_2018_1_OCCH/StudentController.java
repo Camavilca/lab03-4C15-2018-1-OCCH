8a75704c0ad0951fb6bdd190673f52bf2a5a4b43
Camavilca <ocamavilcachavez@gmail.com>
2018-09-11 01:33:10 -0500

    COMMIT 03 CCHO SE MODIFICO EL STUDENTCONTROLLER

--- src/main/java/com/camavilca/lab03/lab03_4C15_2018_1_OCCH/StudentController.java
+++ src/main/java/com/camavilca/lab03/lab03_4C15_2018_1_OCCH/StudentController.java
@@ -1,5 +1,11 @@
 package com.camavilca.lab03.lab03_4C15_2018_1_OCCH;
 
 public class StudentController {
-
+	/*
+	 * ESTA CLASE FUE MODIFICADA POR EL 
+	 * RESPONSABLE DEL PROYECTO 
+	 * CLASE CRADA POR EL COLABORADOR
+	 * */
+	int a; 
+	int b;
 }
--- src/main/java/com/camavilca/lab03/lab03_4C15_2018_1_OCCH/StudentReport.java
+++ src/main/java/com/camavilca/lab03/lab03_4C15_2018_1_OCCH/StudentReport.java
@@ -1,5 +1,10 @@
 package com.camavilca.lab03.lab03_4C15_2018_1_OCCH;
 
 public class StudentReport {
-
+	/*
+	 *---------------------------------------------------------------------------------
+	 * ESTE CLASE FUE CREADA POR ORLANDO CAMAVILCA CHAVEZ
+	 * EL  RESPONSABLE DEL PROYECTO
+	 * --------------------------------------------------------------------------------
+	 * */
 }
