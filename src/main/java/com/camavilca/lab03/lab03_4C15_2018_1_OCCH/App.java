af52aec75b8fabf79373e1bad0d60a5b7a3e3566
Camavilca <ocamavilcachavez@gmail.com>
2018-09-10 23:29:33 -0500

    COMMIT 01 OCCH

---
+++ src/main/java/com/camavilca/lab03/lab03_4C15_2018_1_OCCH/App.java
@@ -0,0 +1,13 @@
+package com.camavilca.lab03.lab03_4C15_2018_1_OCCH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
